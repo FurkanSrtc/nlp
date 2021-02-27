ea9e9c76d49a8c6fa48db1eaebbc021c594c9d5b
oguzhanozdmr <ademoguzhanozdmr@gmail.com>
2021-02-27 22:13:20 +0300

    add clear txt lib

---
+++ clear_txt.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 27 22:07:48 2021
+
+@author: ademoguzhanozdemir
+"""
+
+#pip install html2text
+import nltk
+from html2text import html2text
+
+ 
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
